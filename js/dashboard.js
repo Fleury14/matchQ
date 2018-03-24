701ec1b59d28e1df5eb6291cf4e284937a5a0059
J.R. Ruggiero <fleury14@comcast.net>
2018-03-24 16:04:08 -0700

    card fade

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,17 @@
 const navbar = document.querySelector('.nav-bar ul');
+const cards = document.getElementsByClassName('card');
 
-window.onload = function() { navbar.classList.remove('faded'); }
+window.onload = function() { 
+    navbar.classList.remove('faded'); 
+    console.log(cards);
+    for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove('owned-fade');
+            cards[i].classList.remove('sub-fade');    
+    }
+    // cards.forEach( card => {
+    //     card.classList.remove('owned-fade');
+    //     card.classList.remove('sub-fade');
+    // } );
+    
+    
+};
