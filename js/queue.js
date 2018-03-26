cb9961878bc88642de0f3879bddd2475ebd8b526
J.R. Ruggiero <fleury14@comcast.net>
2018-03-26 11:59:44 -0700

    included active toggle

--- js/queue.js
+++ js/queue.js
@@ -2,6 +2,9 @@
 
 const h3 = document.getElementById('sectionh3');
 const section = document.querySelector('section');
+const activeButton = document.getElementById('activeToggle');
+const activeStatus = document.getElementById('activeStatus');
+let isActive = true;
 
 function toggleHeight() {
     section.classList.toggle('close-add-match');
@@ -12,4 +15,27 @@ function toggleHeight() {
     }
 }
 
+function toggleActive() {
+    if (isActive === true) {
+        activeStatus.classList.remove('success');
+        activeStatus.style.opacity = 0;
+        setTimeout(() => {
+            activeStatus.style.opacity = 1;
+            activeStatus.classList.add('danger');
+            activeStatus.innerHTML = 'No';    
+        },500)
+        isActive = false;
+    } else {
+        activeStatus.classList.remove('danger');
+        activeStatus.style.opacity = 0;
+        setTimeout(() => {
+            activeStatus.style.opacity = 1;
+            activeStatus.classList.add('success');
+            activeStatus.innerHTML = 'Yes';
+        },500)
+        isActive = true;
+    }
+}
+
 section.addEventListener('click', toggleHeight);
+activeButton.addEventListener('click', toggleActive);
