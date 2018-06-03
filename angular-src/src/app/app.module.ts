2b5d6c7b944d5f6a65708cb3afc373aed9ed9bc4
J.R. Ruggiero <fleury14@comcast.net>
2018-06-03 13:11:25 -0700

    updated angular to 6

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule, routingComponents} from './app.routing.module';
-
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent, routingComponents
+    AppComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
