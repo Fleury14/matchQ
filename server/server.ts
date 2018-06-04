12071b1c1018fa82056340b70c79913d6f07ef05
J.R. Ruggiero <fleury14@comcast.net>
2018-06-04 10:36:56 -0700

    added mongoose, connection to database works

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,25 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 import { initAPI } from './api/api';
 
+dotenv.config();
+
+// init app/port
 const app:express.Application = express();
 const port:number = 8080;
+
+// connect to db
+mongoose.connect(process.env.MONGODATABASE);
+// display message on connect
+mongoose.connection.on('connected', () => {
+    console.log('Connected to databse ' + process.env.MONGODATABASE);
+});
+// display message on error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ', err)
+});
 
 initAPI(app);
 
