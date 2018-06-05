123134901c24e392e24a72e1f7c194a95ea676b3
J.R. Ruggiero <fleury14@comcast.net>
2018-06-04 17:13:05 -0700

    feat(api) tournament list functionality added

--- server/api/api.ts
+++ server/api/api.ts
@@ -3,6 +3,7 @@ import { testEndpoint } from "./test-endpoint";
 import { testBodyEndpoint } from "./test-body-endpoint";
 import { addTournament } from "./endpoints/tournaments/addTournament";
 import { deleteTournament } from "./endpoints/tournaments/deleteTournament";
+import { listTournaments } from "./endpoints/tournaments/listTournaments";
 
 export function initAPI (app:Application) {
     app.route('/api/test').get(testEndpoint);
@@ -10,4 +11,5 @@ export function initAPI (app:Application) {
 
     app.route('/api/tournament/add').post(addTournament);
     app.route('/api/tournament/delete').delete(deleteTournament);
+    app.route('/api/tournament/list').get(listTournaments);
 }
