0b2657c525afe30bf44458cea892928b0b0335bb
J.R. Ruggiero <fleury14@comcast.net>
2018-06-13 16:24:36 -0700

    chore(queue): queue component from static

---
+++ angular-src/src/app/components/queue/queue.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-queue',
+    templateUrl: './queue.component.html',
+    styleUrls: [ './queue.component.scss' ]
+})
+
+export class QueueComponent {}
